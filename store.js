702d816d99629b8bdfe15ec8e18fcb5bb9006d73
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-23 13:43:58 +0200

    created the created store function

--- store.js
+++ store.js
@@ -2,12 +2,16 @@ const initial = {
   value: 1,
 };
 
-
 const createStore = (initial) => {
   let state = initial;
 
   const update = (action) => {
-    if (typeof action !== 'function')
+    if (typeof action !== "function") {
       throw new Error("Action must be a function");
+    }
+  };
+
+  return {
+    update:
   }
-}
+};
